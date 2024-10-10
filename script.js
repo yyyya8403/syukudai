f2944b4173327262fd97b311fef898da122fa5b6
yyyya8403 <112771506+yyyya8403@users.noreply.github.com>
2024-10-10 14:59:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const overlay = document.getElementById('overlay');
 const boundingBoxElement = document.getElementById('bounding-box');
 
 // Google Apps ScriptのURLをここに設定
-const spreadsheetUrl = "https://script.google.com/macros/s/AKfycbwmVuv79_MYeWNMBKSwZJK9ud95GEORdSYHcgG1UeSDYkszhSVxpJL9gZJPbDGvrfPFRA/exec";
+const spreadsheetUrl = "https://script.google.com/macros/s/AKfycby3NsJd8iURmYPOZL1BjSt7Bbsh_-JWYrfY7HPl4u5qrUycMuSAX3uJhwXbJizvfqB8cA/exec";
 
 // カメラ映像を表示する処理
 const constraints = {
